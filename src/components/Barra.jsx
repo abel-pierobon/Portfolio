524d72283d6491f75cc5cf91fee306eeb6d542ff
nestorabel.pierobon <nestorabel.pierobon@gmail.com>
2024-07-04 17:12:44 -0300

    colores de menu cambiados

--- src/components/Barra.jsx
+++ src/components/Barra.jsx
@@ -11,34 +11,34 @@ function Barra({
     ingles,
 }) {
     return (
-        <div className=" flex flex-row justify-end fixed top-0 z-10 bg-[#a1b2ff] w-full ">
-            <div className=" flex flex-row justify-end fixed top-0 z-10 bg-[#a1b2ff] w-full ">
+        <div className=" flex flex-row justify-end fixed top-0 z-10 bg-[#463eed] w-full ">
+            <div className=" flex flex-row justify-end fixed top-0 z-10 bg-[#5C61F8] w-full ">
                 <button
-                    className="  px-1 sm:px-2 py-1 mx-1 text-sm sm:text-lg  xl:text-2xl font-semibold hover:font-bold hover:text-[#463eed]"
+                    className="  px-1 sm:px-2 py-1 mx-1 text-sm sm:text-lg  xl:text-2xl text-[#edf2ff] font-semibold hover:font-bold"
                     onClick={handleHome}
                 >
                     {ingles ? idioma.barra.home : "Inicio"}
                 </button>
                 <button
-                    className="  px-2 py-1 mx-1 text-sm sm:text-lg xl:text-2xl font-semibold hover:font-bold hover:text-[#463eed]"
+                    className=" px-1 sm:px-2 py-1 mx-1 text-sm sm:text-lg  xl:text-2xl text-[#edf2ff] font-semibold hover:font-bold"
                     onClick={handleCertificaciones}
                 >
                     {ingles ? idioma.barra.certificaciones : "Certificados"}
                 </button>
                 <button
-                    className="  px-2 py-1 mx-1 text-sm sm:text-lg xl:text-2xl font-semibold hover:font-bold hover:text-[#463eed]"
+                    className=" px-1 sm:px-2 py-1 mx-1 text-sm sm:text-lg  xl:text-2xl text-[#edf2ff] font-semibold hover:font-bold"
                     onClick={handleProyectos}
                 >
                     {ingles ? idioma.barra.proyectos : "Proyectos"}
                 </button>
                 <button
-                    className="  px-2 py-1 mx-1 text-sm sm:text-lg xl:text-2xl font-semibold hover:font-bold hover:text-[#463eed]"
+                    className=" px-1 sm:px-2 py-1 mx-1 text-sm sm:text-lg  xl:text-2xl text-[#edf2ff] font-semibold hover:font-bold"
                     onClick={handleContacto}
                 >
                     {ingles ? idioma.barra.contacto : "Contacto"}
                 </button>
                 <button
-                    className="  px-2 py-1 mx-1 text-sm sm:text-lg xl:text-2xl font-semibold hover:font-bold hover:text-[#463eed]"
+                    className=" px-1 sm:px-2 py-1 mx-1 text-sm sm:text-lg  xl:text-2xl text-[#edf2ff] font-semibold hover:font-bold"
                     onClick={handleIdioma}
                 >
                     <img
